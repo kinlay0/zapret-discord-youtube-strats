4f14a40c5d00c1e635dae733550d8947b7f5f14c
kinlay0 <92862763+kinlay0@users.noreply.github.com>
2026-08-06 11:22:43 +0300

    update github urls in service.bat (for updates)

--- service.bat
+++ service.bat
@@ -378,9 +378,9 @@ chcp 437 > nul
 cls
 
 :: Set current version and URLs
-set "GITHUB_VERSION_URL=https://raw.githubusercontent.com/Flowseal/zapret-discord-youtube/main/.service/version.txt"
-set "GITHUB_RELEASE_URL=https://github.com/Flowseal/zapret-discord-youtube/releases/tag/"
-set "GITHUB_DOWNLOAD_URL=https://github.com/Flowseal/zapret-discord-youtube/releases/latest"
+set "GITHUB_VERSION_URL=https://raw.githubusercontent.com/kinlay0/zapret-discord-youtube-strats/main/.service/version.txt"
+set "GITHUB_RELEASE_URL=https://github.com/kinlay0/zapret-discord-youtube-strats/releases/tag/"
+set "GITHUB_DOWNLOAD_URL=https://github.com/kinlay0/zapret-discord-youtube-strats/releases/latest"
 
 :: Get the latest version from GitHub
 for /f "delims=" %%A in ('powershell -NoProfile -Command "(Invoke-WebRequest -Uri \"%GITHUB_VERSION_URL%\" -Headers @{\"Cache-Control\"=\"no-cache\"} -UseBasicParsing -TimeoutSec 5).Content.Trim()" 2^>nul') do set "GITHUB_VERSION=%%A"
